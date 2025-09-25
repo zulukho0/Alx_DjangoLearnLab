4d413c9e5a7fdf1a15d3ba0e922585eafbb80627
zulukho0 <zulukho0@gmail.com>
2025-09-25 16:56:15 +0200

    Refactor import statement for DetailView in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Book
 from .models import Library
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
+
 
 
 # Create your views here.
