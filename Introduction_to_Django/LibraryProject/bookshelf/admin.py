c6e0b358a869e9daf2de6f1b03bd3b57ba8ec10a
zulukho0 <zulukho0@gmail.com>
2025-09-25 16:16:35 +0200

    register admin to managae Book model, and create superuser

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Book
 
 # Register your models here.
+admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('publication_year',)
+    search_fields = ('title', 'author')
+
+admin.site.register(Book, BookAdmin)
