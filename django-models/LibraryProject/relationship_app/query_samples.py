b584e55ae0231001f6ac961d528702918ebfabc4
zulukho0 <zulukho0@gmail.com>
2025-09-25 16:43:11 +0200

    Refactor query_samples.py to use variables for author and library names

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,11 +1,13 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author = Author.objects.get(name="George Orwell")
+author_name = "George Orwell"
+author = Author.objects.get(name=author_name)
 books_by_author = Book.objects.filter(author=author)
 
 # List all books in a library
-library = Library.objects.get(name="Central Library")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 
 # Retrieve the librarian for a library
