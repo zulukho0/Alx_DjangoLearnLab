544514788d4acf92e3b39ad3fb897f0e1dda4da3
zulukho0 <zulukho0@gmail.com>
2025-09-25 17:02:25 +0200

    Refactor views.py for improved authentication handling

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
 
 
 
