761794b19a2e6c40ffe8882fe0f45b8072f13749
zulukho0 <zulukho0@gmail.com>
2025-09-29 18:55:23 +0200

    refactor: add ExampleForm import to views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect
 from .models import Book
 from .forms import BookForm
+from .forms import ExampleForm
 
 # Create your views here.
 @permission_required('bookshelf.can_create', raise_exception=True)
